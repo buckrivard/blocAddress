523a8d943ef32e0461228bb14df0a3a17f298fc9
Buck Rivard <buckrivard@gmail.com>
2017-03-01 13:57:57 -0800

    entries_2 specs added

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -84,11 +84,37 @@ RSpec.describe AddressBook do
 		end
 	end
 
+	describe "imports from entries_2.csv" do 
+		it "imports the correct number of entries" do
+			book.import_from_csv("entries_2.csv")
+			book_size = book.entries.size
+
+			expect(book_size).to eq 3
+		end
+
+		it "imports the first entry" do
+			book.import_from_csv("entries_2.csv")
+			entry_one = book.entries[0]
+
+			check_entry(entry_one, "Beck", "111-111-1111", "beck@blocmail.com")
+		end
+
+		it "imports the second entry" do
+			book.import_from_csv("entries_2.csv")
+			entry_two = book.entries[1]
+
+			check_entry(entry_two, "Julie", "222-222-2222", "julie@blocmail.com")
+		end
+
+		it "imports the third entry" do 
+			book.import_from_csv("entries_2.csv")
+			entry_three = book.entries[2]
+
+			check_entry(entry_three, "Todd", "333-333-3333", "todd@blocmail.com")
+		end
+	end
+
 end
-
-
-
-
 
 
 
